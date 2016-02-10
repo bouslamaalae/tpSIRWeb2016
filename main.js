c190371578421044240864c566488fc767132806
16012281 <16012281@e007m01.istic.univ-rennes1.fr>
2016-02-10 09:55:33 +0100

    test2

--- main.js
+++ main.js
@@ -11,10 +11,11 @@ ctx.fillStyle = '#F0F0F0'; // set canvas' background color
 ctx.fillRect(0, 0, canvas.width, canvas.height);  // now fill the canvas
 /////
 
+
 // Code temporaire pour tester l'affiche de la vue
 var rec = new Rectangle(10, 20, 50, 100, 5, '#00CCC0');
 rec.paint(ctx);
-var ligne = new Rectangle(10, 20, 50, 100, 5, '#00CCC0');
+var ligne = new Ligne(10, 20, 50, 100, 5, '#00CCC0');
 ligne.paint(ctx);
 // tester également Dessin.
 ////
--- view.js
+++ view.js
@@ -1,2 +1,29 @@
 
 // Implémenter ici les fonctions paint à ajouter dans chacune des classes du modèle.
+Rectangle.prototype.paint = function(ctx) {
+//TODO Manager color
+    ctx.rect(this.x1, this.x2, this.x1+this.epai, this.x2+this.epai);
+    ctx.stroke();
+};
+
+Line.prototype.paint = function(ctx) {
+//TODO Manager color
+
+    ctx.beginPath();
+    ctx.moveTo(this.x1, this.x2);
+    ctx.lineTo(this.y1, this.y2);
+    ctx.stroke();
+
+};
+
+Drawing.prototype.paint = function(ctx) {
+    console.log(this.getForms());
+    ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    this.getForms().forEach(function(eltDuTableau) {
+        // now fill the canvas
+        eltDuTableau.paint(ctx);
+    });
+};
+
+
